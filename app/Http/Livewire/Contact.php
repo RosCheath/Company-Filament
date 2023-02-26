f87e441b8fac21eefcd93a2c6578533d0134bb1d
RosCheat <rossocheat1@gmail.com>
2023-02-26 19:34:59 +0700

    clear code

--- app/Http/Livewire/Contact.php
+++ app/Http/Livewire/Contact.php
@@ -3,27 +3,32 @@
 namespace App\Http\Livewire;
 
 use App\Models\ContactMessage;
-use Filament\Notifications\Notification;
-use Illuminate\Http\Request;
 use Livewire\Component;
-use phpDocumentor\Reflection\Types\This;
 
 class Contact extends Component
 {
-    public $name,$email,$subject,$message;
-    public function store(){
+    public $name;
+
+    public $email;
+
+    public $subject;
+
+    public $message;
+
+    public function store()
+    {
         $contact = new ContactMessage();
         $contact->name = $this->name;
-        $contact->email= $this->email;
-        $contact->subject=$this->subject;
-        $contact->message=$this->message;
+        $contact->email = $this->email;
+        $contact->subject = $this->subject;
+        $contact->message = $this->message;
         $contact->save();
         session()->flash('success', 'Your contact has been send.');
-        $this->reset(['subject','message']);
+        $this->reset(['subject', 'message']);
     }
+
     public function render()
     {
         return view('livewire.contact');
     }
-
 }
